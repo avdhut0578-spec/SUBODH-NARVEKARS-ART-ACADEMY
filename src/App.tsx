4ecdacb72dd8f3dd688c91b41d31c282cf5c85ec
avdhut0578-spec <avdhut0578@gmail.com>
2026-06-19 17:13:48 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
+import VideoCourses from "@/pages/video-courses";
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
+      <Route path="/video-courses" component={VideoCourses} />
       <Route component={NotFound} />
     </Switch>
   );
